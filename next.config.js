964c9c663d90e9e3bb24a026e1655bffccf1bfa6
Allahensus <allanhensus@gmail.com>
2026-08-21 19:21:12 -0300

    fix: bundle prisma dev.db with explicit path tracing for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./prisma/dev.db'],
+    '/*': ['./prisma/dev.db'],
+  },
 };
 
 module.exports = nextConfig;
